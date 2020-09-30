ef6498562ff18bbb90d54b37a291099ad27ff59d
arnab25 <arnabroy2597@gmail.com>
2020-09-30 12:14:21 +0530

    done by arnab roy

---
+++ mavenApp1/src/test/java/com/cg/project/test/AppTest.java
@@ -0,0 +1,28 @@
+package com.cg.project.test;
+
+
+import org.junit.Test;
+
+public class AppTest {
+	@Test
+	   public void test1() {
+		   System.out.println("Test1");
+	   }
+	@Test
+	   public void test2() {
+		   System.out.println("Test2");
+	   }
+	@Test
+	   public void test3() {
+		System.out.println("Test3");
+		  
+	   }
+	@Test
+	   public void test4() {
+		System.out.println("Test4");
+		  
+		  
+	   }
+
+	
+}
